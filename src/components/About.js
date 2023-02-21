4733ce973fd06d1aa83e3e61a7d1cefce09f97e3
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-02-21 17:26:27 -0500

    edited

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,6 @@ import Links from "./Links";
 import user from "../data/user"
 
 function About(props) {
-
-  // const bio = (props) => {
-  //   if (!props.bio) {
-  //   return null
-  //   } else {
-  //     return (
-  //       <p>{props.bio}</p>
-  //     )} 
-  //   }
- 
   return (
     <div id="about">
       <h2>About Me</h2>
--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-
-
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
